37d7b10c3567ab3dc702b7ddaf9af41e4144101d
Soukayna Alala <soukayna.alala45@gmail.com>
2024-10-03 00:08:50 +0100

    created Header and Footer components, and working on CSS

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Footer } from "./components/Footer/Footer.tsx";
+import { Header } from "./components/Header/Header.tsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
