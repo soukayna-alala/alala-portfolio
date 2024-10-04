88ed93d9e20da3001769a2fbf8595940ec6e4325
Soukayna Alala <soukayna.alala45@gmail.com>
2024-10-04 13:42:17 +0100

    finished the CSS and the editing for AboutMe component

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,17 +1,56 @@
+import { Typewriter } from "react-simple-typewriter";
 import styles from "./AboutMe.module.css";
 
 export function AboutMe() {
-  const { paragraph, container } = styles;
+  const {
+    h1,
+    paragraphContainer,
+    imageContainer,
+    paragraph,
+    container,
+    aboutMeImage,
+  } = styles;
 
   return (
     <div className={container}>
-      {/*<h4 className={h4}>About me</h4>*/}
-      <p className={paragraph}>
-        I'm a Junior Frontend developer based in London.
-        <br /> I entered the world of coding since 2022 and <br /> I'm
-        continuously practicing and <br /> working hard to develop and <br />
-        learn more...
-      </p>
+      <div className={paragraphContainer}>
+        <div className={h1}>
+          <Typewriter
+            words={["Front-End Developer"]}
+            loop={1}
+            typeSpeed={120}
+            delaySpeed={1200}
+          />
+        </div>
+        <div className={paragraph}>
+          <p>
+            Hi there,
+            <br /> My name is Soukayna, I'm 24 years old and I live in London.
+            <br />
+            I entered the world of coding since 2022 and I'm continuously
+            <br />
+            practicing and working hard to develop and to learn more...
+            <br />
+            I'm looking to get my first role as a Junior Frontend developer.
+          </p>
+          <p>
+            My goal is to keep learning new things, <br />
+            and to improve my skills everyday, analyze and <br />
+            solve UI and programing issues. <br />
+            Also being able to work independently
+            <br />
+            and in a team.
+          </p>
+        </div>
+      </div>
+
+      <div className={imageContainer}>
+        <img
+          className={aboutMeImage}
+          src="myPicture.jpg"
+          alt="Soukayna Alala's image "
+        />
+      </div>
     </div>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,20 @@
 import styles from "./Header.module.css";
 
 export function Header() {
-  const { name, container, mainContainer } = styles;
+  const { souka, cursor, name, container, mainContainer } = styles;
 
   return (
     <header className={mainContainer}>
-      <div className={name}>Soukayna Alala</div>
+      <div className={name}>
+        <p> Alala</p>
+        <p className={souka}>Soukayna</p>
+      </div>
+
       <div className={container}>
-        <div>About Me</div>
-        <div>Skills</div>
-        <div>Projects</div>
-        <div>Contacts</div>
+        <div className={cursor}>About Me</div>
+        <div className={cursor}>Skills</div>
+        <div className={cursor}>Projects</div>
+        <div className={cursor}>Contacts</div>
       </div>
     </header>
   );
