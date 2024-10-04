4b863600998c76eac498035bfde293a0ef2b114b
Soukayna Alala <soukayna.alala45@gmail.com>
2024-10-04 17:34:51 +0100

    working on CSS on skills component

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,4 +1,13 @@
 import styles from "./Skills.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faReact,
+  faJs,
+  faCss3,
+  faHtml5,
+  faGit,
+} from "@fortawesome/free-brands-svg-icons";
+// import { faJavaScript } from "@fortawesome/free-solid-svg-icons";
 
 export function Skills() {
   const {
@@ -9,6 +18,10 @@ export function Skills() {
     mainContainer,
     skills,
     imgContainer,
+    tsIcon,
+    icons,
+    skill,
+    span,
   } = styles;
 
   return (
@@ -23,12 +36,34 @@ export function Skills() {
       <div className={container}>
         <div className={title}>My Skills</div>
         <div className={skills}>
-          <div>React</div>
-          <div>JavaScript</div>
-          <div>TypeScript</div>
-          <div>HTML</div>
-          <div>CSS</div>
-          <div>GIt</div>
+          <div className={skill}>
+            <FontAwesomeIcon className={icons} icon={faReact} />
+            <span className={span}>React</span>
+          </div>
+          <div className={skill}>
+            <FontAwesomeIcon className={icons} icon={faJs} />
+            <span className={span}>JavaScript</span>
+          </div>
+          <div className={skill}>
+            <img
+              className={tsIcon}
+              src="icons8-typescript.svg"
+              alt="typescript icon"
+            />
+            <span className={span}>TypeScript</span>
+          </div>
+          <div className={skill}>
+            <FontAwesomeIcon className={icons} icon={faHtml5} />
+            <span className={span}>HTML</span>
+          </div>
+          <div className={skill}>
+            <FontAwesomeIcon className={icons} icon={faCss3} />
+            <span className={span}>CSS</span>
+          </div>
+          <div className={skill}>
+            <FontAwesomeIcon className={icons} icon={faGit} />
+            <span className={span}>GIt</span>
+          </div>
         </div>
         <button className={button}>My resume</button>
       </div>
